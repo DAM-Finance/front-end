d3910db31666939d3d84961a38b6d7a99d85903d
BhuiyanSaif18 <saif_lesnar@outlook.com>
2022-08-17 20:10:35 +0600

    Abi files and its address added

---
+++ src/constants/config.ts
@@ -0,0 +1,15 @@
+export const dPrime = "0xf39E86ea5e491339A240120768827e1A1A32a4e1";
+export const lmcv = "0x7B1960Fb005AC409466070f52d8de4143f915a04";
+export const dPrimeJoin = "0xEf75f6E9733384395DdAC1a265977fE6B03C22A8";
+export const LMCVProxy = "0xa2CEa9B8F6Af2d2e20B4dDB66De67cCB98EA5E3c";
+
+
+export const foo = "0xDaf0C8585d57eADBAC974b4dcF6AEE5862FF3d31";
+export const bar = "0x8bE2b54A86ADd13b141c778Fd785653A5a381d3E";
+export const baz = "0xa28905518a893A085a41Ab24dbcb405E5C2AF05c";
+export const fooJoin = "0xE2591840A843C93499B91176C3BDd627483C5165";
+export const barJoin = "0x3cFEA900fd462607CCf32EBA83a780973d9C2F66";
+export const bazJoin = "0xbb2EbebC17CAf0cD430965912632615aF9611273";
+
+
+export const rpcUrl ="https://rpc.api.moonbase.moonbeam.network";
