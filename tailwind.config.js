6f600d6c1f88751a59f37cc43e0bffde7a2c83c0
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-09-05 00:54:07 +0100

    extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        damgray: '#1F212C',
+        damdarkgray: '#151823'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
