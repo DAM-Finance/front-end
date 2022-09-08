58e993cb2901fd72bdef26ddf3decffe2ab97d51
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-09-08 18:27:02 +0100

    DDprime initial design

--- src/components/dashboard/DDPrime.tsx
+++ src/components/dashboard/DDPrime.tsx
@@ -3,7 +3,20 @@ import { FC } from 'react'
 // interface DDPrimeProps {}
 
 const DDPrime: FC = () => {
-  return <div className="bg-damgray rounded-xl p-8 py-8">DDPrime</div>
+  return (
+    <div className="bg-damgray flex rounded-xl p-8 py-8">
+      <div>
+        <div className="text-lg text-gray-300">
+          <span>Meet </span>
+          <span className="font-bold">ddPRIME</span>
+        </div>
+        <div className="text-gray-400 text-sm">Put your LP tokens to work</div>
+      </div>
+      <div className="ml-auto flex justify-center">
+        <button className="text-yellow-200">Start Earning</button>
+      </div>
+    </div>
+  )
 }
 
 export default DDPrime
