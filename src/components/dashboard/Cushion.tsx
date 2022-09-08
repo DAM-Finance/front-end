c4cc43fdf8aaa28aea39a1fbdeb8a2bfb0e8a8d0
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-09-08 17:38:20 +0100

    update cushion

--- src/components/dashboard/Cushion.tsx
+++ src/components/dashboard/Cushion.tsx
@@ -3,7 +3,16 @@ import { FC } from 'react'
 // interface CushionProps {}
 
 const Cushion: FC = () => {
-  return <div className="bg-damgray rounded-xl p-8">Cushion</div>
+  return (
+    <div className="flex flex-col bg-damgray rounded-xl overflow-hidden">
+      <div className="p-6 gap-1">
+        <div className="text-gray-500">Cushion</div>
+        <div className="text-3xl text-orange-300">21%</div>
+        <div className="text-xs text-orange-300">High Risk of Liquidation</div>
+      </div>
+      <button className="bg-damtranspgray rounded-none w-full mt-auto text-yellow-200 py-3">Repay Now</button>
+    </div>
+  )
 }
 
 export default Cushion
