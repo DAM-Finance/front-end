6fb75d36cbaaefc5a9b774ae8481960443d6ff89
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-09-08 17:38:29 +0100

    up dprime

--- src/components/dashboard/DPrime.tsx
+++ src/components/dashboard/DPrime.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react'
+import utils from '../../constants/utils'
 
 // interface DPrimeProps {}
 
 const DPrime: FC = () => {
-  return <div className="bg-damgray rounded-xl p-8">DPrime</div>
+  const dPrimeLogo = utils.getImageSrc('dprime.png')
+  return (
+    <div className="flex flex-col bg-damgray rounded-xl p-6 gap-1">
+      <img height="48px" width="38px" src={dPrimeLogo} alt="dPrime" />
+      <div className="text-gray-500">dPRIME</div>
+      <div className="text-2xl">23,000</div>
+    </div>
+  )
 }
 
 export default DPrime
