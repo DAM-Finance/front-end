5c570cc0c667b159fc7baefcdb2c87e0b147bc35
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-09-20 00:07:27 +0100

    remove unused libs

--- src/components/pages/ComingSoon.tsx
+++ src/components/pages/ComingSoon.tsx
@@ -1,19 +1,7 @@
-import { Stack, Typography } from "@mui/material";
-import { FC } from "react";
+import { FC } from 'react'
 
 const ComingSoon: FC = () => {
-    return ( 
-        <Stack
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            height="100%"
-        >
-            <Typography variant="h1" marginBottom="40px" sx={{ color: "text.primary" }}>
-                Coming Soon
-            </Typography>
-        </Stack>
-     );
+  return <div className="w-full mt-48 flex justify-center items-center text-white">Coming Soon</div>
 }
- 
-export default ComingSoon;
+
+export default ComingSoon
--- src/components/pages/PageNotFound.tsx
+++ src/components/pages/PageNotFound.tsx
@@ -1,17 +1,7 @@
-import { Stack, Typography } from '@mui/material'
 import { FC } from 'react'
 
 const NotFound: FC = () => {
-  return (
-    <Stack direction="column" justifyContent="center" alignItems="center" height="100%">
-      <Typography variant="h1" fontWeight="bold" sx={{ color: 'text.primary' }}>
-        404
-      </Typography>
-      <Typography variant="h6" marginBottom="40px" sx={{ color: 'text.primary' }}>
-        PAGE NOT FOUND
-      </Typography>
-    </Stack>
-  )
+  return <div className="w-full mt-48 flex justify-center items-center text-white">404 - PAGE NOT FOUND :(</div>
 }
 
 export default NotFound
