2c06c3a36573aee2843e36a7d8d4e16df912a16e
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-10-07 02:21:05 +0100

    Text improvement 4c43f5c7d1994ea2b05c93b134cd5b1e

--- src/components/pages/Swap.tsx
+++ src/components/pages/Swap.tsx
@@ -8,7 +8,7 @@ const Swap: FC = () => {
   return (
     <div className="flex p-4 w-full justify-center py-24">
       <div className="flex flex-col max-w-7xl gap-6">
-        <Disclaimer infoTxt="Swap existing stablecoins directly for dPrime at a 1:1 rate, rather than borrowing dPrime." actionTxt="Learn More"></Disclaimer>
+        <Disclaimer infoTxt="Swap existing stablecoins directly for dPrime at a 1:1 rate." actionTxt="Learn More"></Disclaimer>
         <Swaper></Swaper>
       </div>
     </div>
