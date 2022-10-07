1170ec6b677e4941e83334fd0b2d86a15a2f4e51
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-10-07 01:33:35 +0100

    Enable popups to be closed
    4e5bf2f7ea7e4246b3b9f2e8bbde7db7
    9dddc5ae34cb4f7daeb1aba741b23ec4

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -1,21 +1,31 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import utils from '../constants/utils'
 
 interface DisclaimerProps {
   infoTxt: string
   actionTxt?: string
   canClose?: boolean
+  handleClose?: () => void
 }
 
-const Disclaimer: FC<DisclaimerProps> = ({ infoTxt = '', actionTxt = '', canClose = true }: DisclaimerProps) => {
+const Disclaimer: FC<DisclaimerProps> = ({ infoTxt = '', actionTxt = '', canClose = true, handleClose }: DisclaimerProps) => {
+  const [showDisclaimer, setShowDisclaimer] = useState(true)
+  const onClose = () => {
+    setShowDisclaimer(false)
+    handleClose && handleClose()
+  }
   return (
-    <div className="flex flex-row gap-4 bg-damnavygreen rounded-2xl p-6">
-      <div>
-        <div className="text-white ">{infoTxt}</div>
-        {actionTxt && <button className="text-damyellow opacity-50">Learn More</button>}
-      </div>
-      {canClose && <img src={utils.getImageSrc('x.svg')} className="ml-auto mb-auto pl-md" alt="close" />}
-    </div>
+    <>
+      {showDisclaimer && (
+        <div className="flex flex-row gap-4 bg-damnavygreen rounded-2xl p-6">
+          <div>
+            <div className="text-white ">{infoTxt}</div>
+            {actionTxt && <button className="text-damyellow opacity-50">Learn More</button>}
+          </div>
+          {canClose && <img onClick={onClose} src={utils.getImageSrc('x.svg')} width={20} className="ml-auto mb-auto cursor-pointer pl-md" alt="close" />}
+        </div>
+      )}
+    </>
   )
 }
 
