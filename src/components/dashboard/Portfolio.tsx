916f0c1a22e282efa353ee38dd06ea473e6f559e
Harrison Comfort <harrison.comfort@gmail.com>
2022-11-18 13:57:35 +0100

    Update name to d2O from dPRIME

--- src/components/dashboard/Portfolio.tsx
+++ src/components/dashboard/Portfolio.tsx
@@ -16,11 +16,11 @@ const Portfolio: FC<Partial<IPortfolio>> = (portfolio) => {
           <span className="text-2xl">Take your stablecoin liquidity anywhere through </span>
           <span className="text-2xl font-bold">DAM</span>
         </div>
-        <div className="text-gray-500">dPRIME is a scalable, omnichain stablecoin with utility in Dotsama</div>
+        <div className="text-gray-500">d2O is a scalable, omnichain stablecoin with utility in Dotsama</div>
         <div className="flex gap-4">
           <NavLink to="/swap">
             <button className="flex font-bold items-center gap-2 rounded-full py-2 px-6 bg-damyellow text-damgray hover:bg-yellow-200">
-              <span>Get dPRIME</span>
+              <span>Get d2O</span>
             </button>
           </NavLink>
           <button className="rounded-full py-1 px-6 bg-dambackgroundgrayed hover:bg-dambackgroundgrayedhover text-damyellow">Learn More</button>
