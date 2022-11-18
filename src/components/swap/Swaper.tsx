06bd721c0dec88b9d3b28379d43108b878f07201
Harrison Comfort <harrison.comfort@gmail.com>
2022-11-18 14:29:09 +0100

    updated d2O name

--- src/components/swap/Swaper.tsx
+++ src/components/swap/Swaper.tsx
@@ -278,7 +278,7 @@ const Swaper: FC = () => {
         {txState === 'completed' && (
           <div className="flex flex-col gap-2 pt-6">
             <div className="text-md text-damlabelgray">
-              <span>Want to teleport your dPRIME to a different network?</span>
+              <span>Want to teleport your d2O to a different network?</span>
             </div>
             <NavLink to="/teleport">
               <button className="flex items-center justify-center gap-2 rounded-full py-3 px-6 mx-auto bg-damyellow text-damgray hover:bg-yellow-200 font-bold">
