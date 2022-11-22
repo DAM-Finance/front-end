47a026072297b07ae127fac5151b6616910bd2f5
Nuno Tomás <nuno.tomas@mudacorp.com>
2022-11-22 13:10:52 +0000

    Fix Warning

--- src/components/transactions/PendingTransactionList.tsx
+++ src/components/transactions/PendingTransactionList.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { useAppStore } from '../../stores/appStore/appStore'
 import PendingTransaction from './PendingTransaction'
 
