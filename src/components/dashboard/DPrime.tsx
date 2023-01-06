d0fa42f51c9d3628755e2402f2433e043a0a3231
Harrison Comfort <harrison.comfort@gmail.com>
2023-01-06 13:11:02 +0100

    change to your d2o balance

--- src/components/dashboard/DPrime.tsx
+++ src/components/dashboard/DPrime.tsx
@@ -16,7 +16,7 @@ const DPrime: FC<Partial<IPortfolio>> = (props) => {
   return (
     <div className="flex flex-col bg-damgray rounded-xl overflow-hidden" style={{ opacity: isValidNetworkConnected() ? '1' : '0.2' }}>
       <div className="flex flex-col p-6 gap-2 relative">
-        <div className="text-damlabelgray">d2O Balance</div>
+        <div className="text-damlabelgray">Your d2O Balance</div>
         <div className="text-3xl">{appStore.balances.dPrime ? utils.beautifyNumber(utils.format(appStore.balances.dPrime, 2)) : 0}</div>
         {/* <img className="absolute top-2 right-2" width={115} src={utils.getImageSrc('dprimebg.svg')} alt="dPrime background" /> */}
         <img
