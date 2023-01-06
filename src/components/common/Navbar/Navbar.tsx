37aa83a61f4412bd593d71da28f66e83f890a7ab
Harrison Comfort <harrison.comfort@gmail.com>
2023-01-06 13:01:49 +0100

    updated nav bar to get d2O

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: FC<NavbarProps> = () => {
         Dashboard
       </NavLink>
       <NavLink to="/swap" className={({ isActive }) => (isActive ? 'text-white font-bold' : 'text-damNavGray hover:text-gray-300')}>
-        Get
+        Get d2O
       </NavLink>
       {/* <NavLink to="/manage" className={({ isActive }) => (isActive ? 'text-white font-bold' : 'text-damNavGray hover:text-gray-300')}>
         Manage
