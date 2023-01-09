baf5211a8a5a19d67b933eb2a1739e6ba437746d
Harrison Comfort <harrison.comfort@gmail.com>
2023-01-09 10:02:04 +0100

    changed back to usually 15 min

--- src/components/common/PendingTransactions/PendingTransactionsEngine.tsx
+++ src/components/common/PendingTransactions/PendingTransactionsEngine.tsx
@@ -213,8 +213,8 @@ const PendingTransactionsEngine: FC<PendingTransactionsProps> = () => {
     <div className="text-black">
       <WaitingForConfirmationPopup handleClose={() => appStore.setNotifyTransaction(null)} show={showTeleportWaiting()} addTokenOption={false}>
         <div className="text-[14px] text-damlabelgray">
-          <span>Teleporting d2O can take</span>
-          <span className="text-damyellow font-bold">60 minutes.</span>
+          <span>Teleporting d2O should take</span>
+          <span className="text-damyellow font-bold">15 minutes.</span>
         </div>
       </WaitingForConfirmationPopup>
 
@@ -233,7 +233,7 @@ const PendingTransactionsEngine: FC<PendingTransactionsProps> = () => {
           setShowDevSwitch(true)
         }}
         show={showTeleportInflight()}
-        message="Step 2/3: Teleportation in flight between origin and destination! It can take 60 minutes."
+        message="Step 2/3: Teleportation in flight between origin and destination! It usually takes 15 minutes."
         txLink={appStore.notifyTransaction?.lzScan || ''}
         imgName="teleport-progress.svg"
       >
