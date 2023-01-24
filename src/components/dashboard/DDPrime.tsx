83f0647ecd28b390816ab187ec30e7a991876978
Harrison Comfort <harrison.comfort@gmail.com>
2023-01-24 08:01:24 -0500

    updated links

--- src/components/dashboard/DDPrime.tsx
+++ src/components/dashboard/DDPrime.tsx
@@ -18,7 +18,9 @@ const DDPrime: FC = () => {
       <div>
         <div className="text-2xl text-white">
           <span>Meet </span>
-          <span className="font-bold">dReservoir</span>
+          <a href="https://docs.dam.finance/concepts/dreservoir" rel="noreferrer">
+          <button className="font-bold">dReservoir</button>
+          </a>
         </div>
         <div className="text-damlabellightgray text-sm">Teleport d2o from Ethereum to Polkadot</div>
       </div>
