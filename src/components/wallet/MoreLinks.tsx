c665079d1f3708d252fb0efda39562fc10a0a28d
Harrison Comfort <harrison.comfort@gmail.com>
2023-01-24 15:51:06 -0500

    fix discord link

--- src/components/wallet/MoreLinks.tsx
+++ src/components/wallet/MoreLinks.tsx
@@ -27,7 +27,7 @@ const MoreLinks: FC = () => {
     docs: { label: 'Docs', url: 'https://docs.dam.finance' },
     github: { label: 'GitHub', url: 'https://github.com/DAM-Finance' },
     audit: {label: 'Audit', url: 'https://github.com/HalbornSecurity/PublicReports/tree/master/Solidity%20Smart%20Contract%20Audits/DAMfinance%20Audits' },
-    discord: { label: 'Discord', url: 'https://t.co/MObW17EWXX' },
+    discord: { label: 'Discord', url: 'https://discord.gg/xGB4pyU4Jp' },
     twitter: { label: 'Twitter', url: 'https://twitter.com/DAM_Finance' },
     logout: {
       label: 'Disconnect',
