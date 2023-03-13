67414e6b7363d7d607824f63b81552db1ff75597
Noah Foltz <nofo@dam.finance>
2023-03-13 08:45:28 -0500

    Reverting usdc changes

--- src/components/pages/Teleport.tsx
+++ src/components/pages/Teleport.tsx
@@ -143,10 +143,10 @@ const Teleport: FC = () => {
                 onClick={() => teleportTo(amount, destinationNetwork)}
                 className="flex items-center w-full justify-center disabled:opacity-50 disabled:cursor-not-allowed gap-2 rounded-full py-4 px-6 text-black font-bold bg-gradient-to-r from-[#7742CD] to-[#F1DD79] hover:from-[#8458cc] hover:to-[#ebdd9c]"
                 style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}
-                disabled={destinationNetwork.chainId === '0x504' || isBelowZero() || isAboveBalance()}
+                disabled={isBelowZero() || isAboveBalance()}
               >
                 <img src={utils.getImageSrc('teleport.svg')} alt="teleport" />
-                {isAboveBalance() ? 'Insufficient balance' : destinationNetwork.chainId === '0x504' ? 'Teleport temporary disabled' : 'Teleport'}
+                {isAboveBalance() ? 'Insufficient balance' : 'Teleport'}
               </button>
               <div className="flex justify-center items-center gap-2">
                 <img src={utils.getImageSrc('warning.svg')} alt="" />
