39eab3bbd5e6e3f0d5ec627982be343c0f523153
Noah Foltz <nofo@dam.finance>
2023-04-17 10:51:54 -0500

    updates

--- src/components/pages/Teleport.tsx
+++ src/components/pages/Teleport.tsx
@@ -190,7 +190,7 @@ const Teleport: FC = () => {
                 onClick={() => teleportTo(amount, destinationNetwork)}
                 className="flex items-center w-full justify-center disabled:opacity-50 disabled:cursor-not-allowed gap-2 rounded-full py-4 px-6 text-black font-bold bg-gradient-to-r from-[#7742CD] to-[#F1DD79] hover:from-[#8458cc] hover:to-[#ebdd9c]"
                 style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}
-                // disabled={isBelowZero() || isAboveBalance()}
+                disabled={isBelowZero() || isAboveBalance()}
               >
                 <img src={utils.getImageSrc('teleport.svg')} alt="teleport" />
                 {isAboveBalance() ? 'Insufficient balance' : 'Teleport'}
