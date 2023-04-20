3e0626dab7dd6c089a1d7fab070b5e577269a517
Noah Foltz <nofo@dam.finance>
2023-04-20 11:01:41 -0500

    astar moonbeam disabled

--- src/components/pages/Teleport.tsx
+++ src/components/pages/Teleport.tsx
@@ -146,6 +146,13 @@ const Teleport: FC = () => {
     return Number(amount) > Number(dPrimeBalance)
   }
 
+  const isntAstarMoonbeam = () => {
+    if((originNetwork.id === 1284 && destinationNetwork.id === 592) || (originNetwork.id === 592 && destinationNetwork.id === 1284)){
+      return false;
+    }
+    return true;
+  }
+
   return (
     <div className="flex p-4 w-full justify-center lg:py-24 py-12">
       <div className="flex flex-col max-w-7xl gap-6">
@@ -191,10 +198,10 @@ const Teleport: FC = () => {
                 onClick={() => teleportTo(amount, destinationNetwork)}
                 className="flex items-center w-full justify-center disabled:opacity-50 disabled:cursor-not-allowed gap-2 rounded-full py-4 px-6 text-black font-bold bg-gradient-to-r from-[#7742CD] to-[#F1DD79] hover:from-[#8458cc] hover:to-[#ebdd9c]"
                 style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}
-                disabled={isBelowZero() || isAboveBalance()}
+                disabled={isBelowZero() || isAboveBalance() || isntAstarMoonbeam()}
               >
                 <img src={utils.getImageSrc('teleport.svg')} alt="teleport" />
-                {isAboveBalance() ? 'Insufficient balance' : 'Teleport'}
+                {isntAstarMoonbeam() ? isAboveBalance() ? 'Insufficient balance' : 'Teleport' :  'Astar <-> Moonbeam coming soon' }
               </button>
               <div className="flex justify-center items-center gap-2">
                 <img src={utils.getImageSrc('warning.svg')} alt="" />
