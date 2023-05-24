35e220335f2ab0a13872977a508f8eda0a9432fa
Noah Foltz <nofo@dam.finance>
2023-05-24 09:06:35 -0500

    update

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -160,7 +160,7 @@ export const supportedTokens: ISupportedTokensMap = {
   }
 }
 
-export const estimateForBurnFee: number = 0.7444444
+export const estimateForBurnFee: number = 0.2444444
 export const burnFee: number = 0.25002
 
 export const astarHyperlaneMetadata: ChainMetadata = {
